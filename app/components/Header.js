5719d91eddd288f31313a9daa86be266f13e952d
Matthew Elphick <matt.a.elphy@gmail.com>
2017-05-14 01:05:11 +0100

    :wrench: Fix linting

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import { IndexLink, Link } from 'react-router';
+import React from 'react'
+import { IndexLink, Link } from 'react-router'
 import { connect } from 'react-redux'
-import { logout } from '../actions/auth';
+import { logout } from '../actions/auth'
 
 class Header extends React.Component {
-  handleLogout(event) {
-    event.preventDefault();
-    this.props.dispatch(logout());
+  handleLogout (event) {
+    event.preventDefault()
+    this.props.dispatch(logout())
   }
 
-  render() {
+  render () {
     const rightNav = this.props.token ? (
-      <ul className="list-inline">
-          <li>
-            <img className="avatar" src={this.props.user.picture || this.props.user.gravatar}/>
-            {' '}{this.props.user.name || this.props.user.email || this.props.user.id}{' '}
-          </li>
-          <li><Link to="/account">My Account</Link></li>
-          <li><a href="#" onClick={this.handleLogout.bind(this)}>Logout</a></li>
+      <ul className='list-inline'>
+        <li>
+          <img className='avatar' src={this.props.user.picture || this.props.user.gravatar} />
+          {' '}{this.props.user.name || this.props.user.email || this.props.user.id}{' '}
+        </li>
+        <li><Link to='/account'>My Account</Link></li>
+        <li><a href='#' onClick={this.handleLogout.bind(this)}>Logout</a></li>
       </ul>
     ) : (
-      <ul className="list-inline">
-          <li><Link to="/login">Log in</Link></li>
-          <li><Link to="/signup">Sign up</Link></li>
+      <ul className='list-inline'>
+        <li><Link to='/login'>Log in</Link></li>
+        <li><Link to='/signup'>Sign up</Link></li>
       </ul>
-    );
+    )
     return (
-      <div className="container">
-        <ul className="list-inline">
-          <li><IndexLink to="/">Home</IndexLink></li>
-          <li><Link to="/contact">Contact</Link></li>
+      <div className='container'>
+        <ul className='list-inline'>
+          <li><IndexLink to='/'>Home</IndexLink></li>
+          <li><Link to='/contact'>Contact</Link></li>
         </ul>
         {rightNav}
       </div>
-    );
+    )
   }
 }
 
@@ -41,7 +41,7 @@ const mapStateToProps = (state) => {
   return {
     token: state.auth.token,
     user: state.auth.user
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header)
--- models/User.js
+++ models/User.js
@@ -1,17 +1,19 @@
-var crypto = require('crypto');
-var bcrypt = require('bcrypt-nodejs');
-var mongoose = require('mongoose');
+/* eslint-disable handle-callback-err */
+
+var crypto = require('crypto')
+var bcrypt = require('bcrypt-nodejs')
+var mongoose = require('mongoose')
 
 var schemaOptions = {
   timestamps: true,
   toJSON: {
     virtuals: true
   }
-};
+}
 
 var userSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true},
+  email: { type: String, unique: true },
   password: String,
   passwordResetToken: String,
   passwordResetExpires: Date,
@@ -24,41 +26,41 @@ var userSchema = new mongoose.Schema({
   google: String,
   github: String,
   vk: String
-}, schemaOptions);
+}, schemaOptions)
 
-userSchema.pre('save', function(next) {
-  var user = this;
-  if (!user.isModified('password')) { return next(); }
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      user.password = hash;
-      next();
-    });
-  });
-});
+userSchema.pre('save', function (next) {
+  var user = this
+  if (!user.isModified('password')) { return next() }
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      user.password = hash
+      next()
+    })
+  })
+})
 
-userSchema.methods.comparePassword = function(password, cb) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-    cb(err, isMatch);
-  });
-};
+userSchema.methods.comparePassword = function (password, cb) {
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    cb(err, isMatch)
+  })
+}
 
-userSchema.virtual('gravatar').get(function() {
+userSchema.virtual('gravatar').get(function () {
   if (!this.get('email')) {
-    return 'https://gravatar.com/avatar/?s=200&d=retro';
+    return 'https://gravatar.com/avatar/?s=200&d=retro'
   }
-  var md5 = crypto.createHash('md5').update(this.get('email')).digest('hex');
-  return 'https://gravatar.com/avatar/' + md5 + '?s=200&d=retro';
-});
+  var md5 = crypto.createHash('md5').update(this.get('email')).digest('hex')
+  return 'https://gravatar.com/avatar/' + md5 + '?s=200&d=retro'
+})
 
 userSchema.options.toJSON = {
-  transform: function(doc, ret, options) {
-    delete ret.password;
-    delete ret.passwordResetToken;
-    delete ret.passwordResetExpires;
+  transform: function (doc, ret, options) {
+    delete ret.password
+    delete ret.passwordResetToken
+    delete ret.passwordResetExpires
   }
-};
+}
 
-var User = mongoose.model('User', userSchema);
+var User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
